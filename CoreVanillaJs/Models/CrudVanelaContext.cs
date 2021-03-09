88c9d315792011c05cc98f5092549ed1ba214ae1
Brayan Monge <brayanmonge12@gmail.com>
2021-03-09 18:43:23 -0500

    Coneeccion con la base de datos

---
+++ CoreVanillaJs/Models/CrudVanelaContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CoreVanillaJs.Models
+{
+    public partial class CrudVanelaContext : DbContext
+    {
+        public CrudVanelaContext()
+        {
+        }
+
+        public CrudVanelaContext(DbContextOptions<CrudVanelaContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Persona> Persona { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=localhost;Database=CrudVanela;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Persona>(entity =>
+            {
+                entity.ToTable("persona");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Edad).HasColumnName("edad");
+
+                entity.Property(e => e.Nombre)
+                    .HasColumnName("nombre")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
